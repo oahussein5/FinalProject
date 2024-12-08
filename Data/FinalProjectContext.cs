9e21133f61de1a517d499f2338b23057853ab55e
Omar <Omar@OAHDESKTOP>
2024-12-07 23:07:32 -0600

    Add project files.

---
+++ Data/FinalProjectContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FinalProject.Models;
+
+namespace FinalProject.Data
+{
+    public class FinalProjectContext : DbContext
+    {
+        public FinalProjectContext (DbContextOptions<FinalProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FinalProject.Models.Application> Application { get; set; } = default!;
+        public DbSet<FinalProject.Models.DogProfile> DogProfile { get; set; } = default!;
+        public DbSet<FinalProject.Models.User> User { get; set; } = default!;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FinalProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }  // Primary Key
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
