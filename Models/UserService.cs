bb0f733bc51fdf5b209ab31d7f55c0dc42867c3d
Omar <omar.hussein@ndsu.edu>
2024-12-08 14:25:33 -0600

    Switched User Page, needs more tweaking to enable to to function properly

---
+++ Models/UserService.cs
@@ -0,0 +1,32 @@
+﻿using FinalProject.Data;
+using FinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class UserService
+    {
+        private readonly FinalProjectContext _context;
+
+        public UserService(FinalProjectContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateUserAsync(User user)
+        {
+            try
+            {
+                _context.User.Add(user); // Assuming you have a DbSet<User> in your ApplicationDbContext
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
